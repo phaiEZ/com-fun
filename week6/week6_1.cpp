b61b01dec4a95899502b1592d6f79b95d09c7320
phai <51281702+phaiEZ@users.noreply.github.com>
2020-09-20 21:46:38 +0700

    Update week6_1.cpp

--- week6/week6_1.cpp
+++ week6/week6_1.cpp
@@ -1,21 +1,27 @@
 #include<stdio.h>
 float x,y,z,num;
-int j,i,k;
+int j,i,k,t,l;
 
-
-int main(){
-    scanf("%f",&x);
+int check(float x){
     k = 1;
     z = x;
     i = x;
-    while(1){
-        if (z == i){
-            break;
-        }
+    while(z!=i){
         z = x * k;
         i = z;
         k += 1;
     }
     printf("%.0f / %d\n",z,k-1);
+    return 0;
+}
+
+
+int main(){
+    
+    scanf("%d",&t);
+    for (l = 0; l < t; l++){
+        scanf("%f",&num);
+        check(num);
+    }
     return 0;
 }
