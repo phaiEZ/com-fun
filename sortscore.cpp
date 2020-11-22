b1c077290a32d87ce39dd6452be0f3ef94d62ac8
phai <51281702+phaiEZ@users.noreply.github.com>
2020-11-22 22:26:40 +0700

    Create sortscore.cpp

---
+++ sortscore.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+struct student
+{
+    int id;
+    int point;
+    int rating;
+};
+vector <student> Stu;
+int n,a,b;
+
+void sortscore(){
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j){
+            if (Stu[i].point < Stu[j].point) {
+                a =  Stu[i].point;
+                b = Stu[i].id;
+
+                Stu[i].point = Stu[j].point;
+                Stu[i].id = Stu[j].id;
+
+                Stu[j].point = a;
+                Stu[j].id = b;
+            }
+        }
+    }
+}
+
+int main(){
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++){
+       student c;
+       scanf("%d",&c.id);
+       scanf("%d",&c.point);
+       Stu.push_back(c);
+    }
+    sortscore();
+    for (int i = 0; i <  Stu.size(); i++){
+        printf("id : %d ",Stu[i].id);
+		printf("point : %d\n",Stu[i].point);
+	}
+    return 0;
+}   
